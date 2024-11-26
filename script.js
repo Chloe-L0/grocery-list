d79c3ec08e2e56803749c5a01dd7a830b2431252
Chloe-O <chloeosbourne@yahoo.co.uk>
2024-11-26 18:38:08 +0000

    init

---
+++ script.js
@@ -0,0 +1,43 @@
+let shoppingListContainer = document.getElementById("shopping-list");
+let listItem = document.querySelectorAll(".list-item");
+let itemName = document.querySelectorAll("item-name");
+let itemQuantity = document.querySelectorAll("item-quantity");
+let itemCategory = document.querySelectorAll("item-category");
+
+let itemQuantityInput = document.getElementById("quantityInput");
+let itemNameInput = document.getElementById("nameInput");
+let itemCategoryInput = document.getElementById("categoryInput");
+let itemSubmitBtn = document.getElementById("itemSubmit");
+
+const itemCategoryArr = [
+  "Produce 🥦",
+  "Meat & Fish 🍗",
+  "Bakery 🍞",
+  "Pantry 🥫",
+  "Confectionary 🍭",
+  "Diary 🧀",
+  "Frozen ❄",
+  "Household 🧹",
+  "Misc ❔",
+];
+
+//populate grocery categories
+
+itemCategoryArr.forEach((i) => {
+  itemCategoryInput.innerHTML += `<option> ${i}</option>`;
+});
+
+console.log(itemCategoryInput.innerHTML);
+
+itemSubmitBtn.addEventListener("click", (e) => {
+  e.preventDefault;
+
+  //clear input information
+  itemQuantityInput.innerHTML = "";
+  itemNameInput.innerHTML = "";
+  itemCategory.innerHTML = "";
+
+  //
+});
+
+const submitListItem = function () {};
